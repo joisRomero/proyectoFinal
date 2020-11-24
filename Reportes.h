7d89a85b4d4a2cab6c1cf7a2e4f9902fa6d65204
joisRomero <joseluis0651@gmail.com>
2020-11-23 22:31:43 -0500

    Termine mi reporte (joisRomero) SOMOS EL EXITO!!!!!:D

--- Reportes.h
+++ Reportes.h
@@ -252,12 +252,200 @@ void reporteRodo(ListaTrabajador &listaTrab){
         cout << "\n\n\t\tNo se ha seleccionado ningun trabajador";
     }
 
-
-
-
     fflush(stdin);
     getch();
 }
+
+/*
+    Reporte de Romero Olivera Jose Luis
+*/
+void reporteJoseRomero(ListaTrabajador &listaTrab) {
+    int cantPersonas;
+    float maximo = (listaTrab.numTrabaj/2);
+    system("cls");
+    interfazmenu("          :: SISTEMA DE CALIFICACION ::");
+    cout << endl << endl;
+
+    cantPersonas = leeEntero("\tIngrese numero de trabjadores a mostrar: ", 2, maximo);
+
+    float puntajeLista[listaTrab.numTrabaj];
+    float puntajeLista2[listaTrab.numTrabaj];
+
+    float puntajeBuenos[cantPersonas], puntajeMalos[cantPersonas];
+
+    float listaPuntajeOr1[listaTrab.numTrabaj], auxNum;
+
+    int acumAsistencias, acumFaltas, acumJusti, acumTardan, j = 0;
+    float acumDescuentos = 0.0, acumBonificaciones = 0.0;
+    float menor, mayor;
+
+    iniciaVR(puntajeLista,listaTrab.numTrabaj);
+    iniciaVR(puntajeLista2,listaTrab.numTrabaj);
+    iniciaVR(listaPuntajeOr1,listaTrab.numTrabaj);
+
+    Trabajador trabajadoresBuen[cantPersonas];
+    Trabajador trabajadoresRegu[cantPersonas];
+    Trabajador auxTrabajador;
+
+
+    for (NodoTrabajador *Aux = listaTrab.cab; Aux != NULL; Aux = Aux->sgte) {
+        acumAsistencias = 0;
+        acumFaltas = 0;
+        acumJusti = 0;
+        acumTardan = 0;
+        for (int i = 0; i < Aux->trab.pagos.listaAsistencia.num; i++) {
+            acumAsistencias += Aux->trab.pagos.listaAsistencia.datos[i].asistencia;
+            acumFaltas += Aux->trab.pagos.listaAsistencia.datos[i].falta;
+            acumJusti += Aux->trab.pagos.listaAsistencia.datos[i].justificacion;
+            acumTardan += Aux->trab.pagos.listaAsistencia.datos[i].tardanza;
+        }
+        puntajeLista[j] = acumAsistencias - acumFaltas + float(acumJusti/2) - float(acumTardan/2);
+        listaPuntajeOr1[j] = puntajeLista[j];
+        j++;
+    }
+
+    for (int i = 0; i < listaTrab.numTrabaj - 1; i++) {
+        for (int k = i + 1; k < listaTrab.numTrabaj; k++) {
+            if(listaPuntajeOr1[i] < listaPuntajeOr1[k]) {
+                auxNum = listaPuntajeOr1[i];
+                listaPuntajeOr1[i] = listaPuntajeOr1[k];
+                listaPuntajeOr1[k] = auxNum;
+            }
+        }
+    }
+
+    int k = 0;
+    int band;
+    for (int i = 0; i < cantPersonas; i++) {
+        j = 0;
+        band = 0;
+        for(NodoTrabajador *Aux = listaTrab.cab; Aux != NULL && band == 0; Aux = Aux->sgte) {
+            if (listaPuntajeOr1[i] == puntajeLista[j]){
+                band = 1;
+                trabajadoresBuen[k] = Aux->trab;
+                k++;
+            }
+            j++;
+        }
+    }
+
+    k = 0;
+    for (int i = listaTrab.numTrabaj-1; i > listaTrab.numTrabaj-(cantPersonas+1); i--) {
+        j = 0;
+        band = 0;
+        for(NodoTrabajador *Aux = listaTrab.cab; Aux != NULL && band == 0; Aux = Aux->sgte) {
+            if (listaPuntajeOr1[i] == puntajeLista[j]){
+                band = 1;
+                trabajadoresRegu[k] = Aux->trab;
+                k++;
+            }
+            j++;
+        }
+    }
+    system("cls");
+    interfazmenu("          :: SISTEMA DE CALIFICACION ::");
+    cout << endl << endl;
+    cout << "\n\tLista de trabajadores eficientes: \n\n";
+    gotoxy(25,11);
+    cout << "DNI";
+    gotoxy(44,11);
+    cout << "NOMBRE";
+    gotoxy(85,11);
+    cout << "GR. INSTRUCCION";
+    int x = 12;
+    for (int i = 0; i < cantPersonas; i++) {
+        gotoxy(25,x);
+        cout << trabajadoresBuen[i].dni;
+        gotoxy(44,x);
+        cout << trabajadoresBuen[i].nombre;
+        gotoxy(85,x);
+        cout << TablaGradoInstruccion[trabajadoresBuen[i].gradoInstruccion-1];
+        x++;
+    }
+    x+=2;
+    cout << "\n\n\tLista de trabajadores deficientes: ";
+    gotoxy(25,x);
+    cout << "DNI";
+    gotoxy(44,x);
+    cout << "NOMBRE";
+    gotoxy(85,x);
+    cout << "GR. INSTRUCCION";
+    x++;
+    for (int i = 0; i < cantPersonas; i++) {
+        gotoxy(25,x);
+        cout << trabajadoresRegu[i].dni;
+        gotoxy(44,x);
+        cout << trabajadoresRegu[i].nombre;
+        gotoxy(85,x);
+        cout << TablaGradoInstruccion[trabajadoresRegu[i].gradoInstruccion-1];
+        x++;
+    }
+
+
+
+    for (int i = 0; i < cantPersonas; i++) {
+        acumBonificaciones = 0;
+        acumDescuentos = 0;
+        for(int k = 0; k < trabajadoresBuen->pagos.listaSueldo.num ; k++){
+            acumBonificaciones += trabajadoresBuen[i].pagos.listaSueldo.datos[k].bonificacion;
+            acumDescuentos += trabajadoresBuen[i].pagos.listaSueldo.datos[k].descuento;
+        }
+        puntajeBuenos[i] = acumBonificaciones - acumDescuentos;
+
+        if( i == 0){
+            mayor = puntajeBuenos[i];
+        } else {
+            if (mayor <puntajeBuenos[i]){
+                mayor = puntajeBuenos[i];
+            }
+        }
+        acumBonificaciones = 0;
+        acumDescuentos = 0;
+        for(int k = 0; k < trabajadoresRegu[i].pagos.listaSueldo.num ; k++){
+            acumBonificaciones = trabajadoresRegu[i].pagos.listaSueldo.datos[k].bonificacion;
+            acumDescuentos = trabajadoresRegu[i].pagos.listaSueldo.datos[k].descuento;
+        }
+        puntajeMalos[i] = acumBonificaciones - acumDescuentos;
+
+        if( i == 0){
+            menor =  puntajeMalos[i];
+        } else {
+            if (menor >  puntajeMalos[i]){
+                menor =  puntajeMalos[i];
+            }
+        }
+    }
+
+    x+=3;
+    gotoxy(10,x);
+    cout << "Trabajador mas eficiente: ";
+    x++;
+    for (int i = 0; i < cantPersonas; i++) {
+        if (mayor == puntajeBuenos[i]){
+            gotoxy(20,x);
+            cout << "Nombre: "<< trabajadoresBuen[i].nombre;
+            x++;
+            gotoxy(20,x);
+            cout << "Puntaje: " << puntajeBuenos[i];
+            x+=2;
+        }
+    }
+
+    for (int i = 0; i < cantPersonas; i++) {
+        if (menor == puntajeMalos[i]){
+            gotoxy(10,x);
+            cout << "Trabajador mas deficiente: ";
+            x++;
+            gotoxy(20,x);
+            cout << "Nombre: "<< trabajadoresRegu[i].nombre;
+            x++;
+            gotoxy(20,x);
+            cout << "Puntaje: " << puntajeMalos[i]<< endl;
+        }
+    }
+    getch();
+}
+
 void menuReportes(ListaTrabajador &lstTrab){
     int opc;
     do {
@@ -288,7 +476,7 @@ void menuReportes(ListaTrabajador &lstTrab){
                 fflush(stdin);
                 break;
             case 2:
-                //reporte JoisRomero;
+                reporteJoseRomero(listaTrab);
                 break;
             case 3:
                 gotoxy(47,23);
